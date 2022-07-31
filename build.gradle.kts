1e904356ae893a79ab73d5dc322cc0ce5523a0cb
vitalir2 <vitalyi100@mail.ru>
2022-07-31 17:59:28 +0300

    Init projects

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "1.6.20" apply false
+    kotlin("multiplatform") version "1.6.20" apply false
+}
+
+group = "io.vitalir"
+version = "0.0.1-alpha"
+
+repositories {
+    mavenCentral()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "untitled"
+
+include(":kodex-plugin")
+include(":kodex-data-api")
+include(":kodex-data-figma")
+include(":kodex-gen-api")
+include(":kodex-gen-android")
+include(":kodex-manager")
